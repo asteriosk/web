700da3b5ad90f8f751a7d46285da53415e07a24c
Asterios Katsifodimos <asteriosk@gmail.com>
2026-04-17 10:10:15 +0200

    Bug in bibtex

--- cv/build.sh
+++ cv/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")/output"
+pdflatex -interaction=nonstopmode asterios.katsifodimos.tex
 biber asterios.katsifodimos
 pdflatex -interaction=nonstopmode asterios.katsifodimos.tex
 pdflatex -interaction=nonstopmode asterios.katsifodimos.tex
